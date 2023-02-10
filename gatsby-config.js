d547a686accf997d0cde038dcef5a4006b0ee70b
Jelle Rienstra <jrienstra86@gmail.com>
2023-02-10 22:57:46 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Korfbalkids`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "fb75e2923c47d46106937a36f4fe11"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
