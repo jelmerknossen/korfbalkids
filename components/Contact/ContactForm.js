7bc3993177d6567de621f4eaf450d1afde72f625
Jelle Rienstra <jrienstra86@gmail.com>
2023-02-24 13:36:02 +0100

    testing

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -30,71 +30,24 @@ const ContactForm = () => {
     <>
       <div className="contact-form pb-70">
         <form name="contact" method="POST" data-netlify="true">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Naam"
-                    className="form-control"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="email"
-                    placeholder="E-mail"
-                    className="form-control"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="number"
-                    placeholder="Telefoonnummer"
-                    className="form-control"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="form-group">
-                  <input
-                    type="text"
-                    name="subject"
-                    placeholder="Onderwerp"
-                    className="form-control"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-lg-12 col-md-12">
-                <div className="form-group">
-                  <textarea
-                    name="text"
-                    cols="30"
-                    rows="6"
-                    placeholder="Bericht..."
-                    className="form-control"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="col-lg-12 col-sm-12">
-                <button type="submit" className="btn btn-primary">
-                  Verstuur bericht
-                </button>
-              </div>
-            </div>
-          </div>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </div>
     </>
